566896f90263cdf76bf294c0fc351bbadf617fe2
DIJOOP <dijoopdas@gmail.com>
2022-06-23 21:50:30 +0530

    server error fixed

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const app= require("./app")
 const dotenv=require("dotenv");
 const connectDataBase = require("./config/database");
 const cloudinary = require ("cloudinary")
+const path=require("path")
 
 // CONFIG
 dotenv.config({path:"config/.env"})
@@ -19,11 +20,10 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 })
 
-if(process.env.NODE_ENV=="production"){
+if(process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging'){
   app.use(express.static("frontend/build"))
-  const path=require("path")
   app.get('*',(req,res)=>{
-    res.sendFile(path.resolve(_dirname,'frontend','buid','index.html'))
+    res.sendFile(path.join(_dirname + 'frontend/build/index.html'))
   })
 }
 
