efa16542e49adab8344a9f89963b280bcc050d66
DIJOOP <dijoopdas@gmail.com>
2022-06-24 10:42:45 +0530

    server error fixe6

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ cloudinary.config({
 if(process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging'){
   app.use(express.static("frontend/build"))
   app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname +'frontend/build/index.html'))
+    res.sendFile(path.resolve(__dirname ,'frontend','build','index.html'))
   })
 }
 
